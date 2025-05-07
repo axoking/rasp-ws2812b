8407dbb7e7737c2c00a82505ee029a87e2038a05
AxolotlKing <>
2025-05-07 23:01:05 +0200

    Added the simplified byte_to_spi_bytes function

--- src/wire_protocol.rs
+++ src/wire_protocol.rs
@@ -1,7 +1,16 @@
+// New method to convert byte to spi bytes
+pub fn byte_to_spi_bytes(byte: u8) -> [u8; 3] {
+	[
+		0b00100100 + (byte<<1&0b10) + (byte<<3&0b10000) + (byte<<5&0b10000000),
+		0b01001001 + (byte>>1&0b100) + (byte<<1&0b100000),
+		0b10010010 + (byte>>5&0b1) + (byte>>3&0b1000) + (byte>>1&0b1000000)
+	]
+}
+
 // Convert panel bits into their SPI counterparts
 // 0 -> 001
 // 1 -> 011
-pub fn byte_to_spi_bytes(input: u8) -> [u8; 3] {
+pub fn byte_to_spi_bytes_old(input: u8) -> [u8; 3] {
     // first convert the u8 to 24 bits
     let mut bool_array = [false; 24];
     for bit_index in 0..8 {
